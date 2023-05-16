142436f9b8f85a3194b3412bb255caed5b004394
rupesh kumar <rudhg35@gmail.com>
2023-05-16 20:38:14 +0530

    eCommers

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport =  require('passport');
+const LocalStrategy =  require('passport-local');
+const User = require('./models/user');
+const seedDB = require('./seed')
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/shopping-g2-app')
+    .then(() => console.log('DB Connected'))
+    .catch((err) => console.log(err));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+const sessionConfig = {
+    secret: 'weneedsomebettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly:true,
+        expires:Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+// seedDB();
+
+// Routes require
+const productRoutes = require('./routes/product');
+const reviewRoutes = require('./routes/review');
+const authRoutes = require('./routes/auth');
+const cartRoutes = require('./routes/cart');
+
+
+app.get('/' , (req,res)=>{
+    res.render('home');
+})
+
+// middle express
+app.use(productRoutes);
+app.use(reviewRoutes);
+app.use(authRoutes);
+app.use(cartRoutes);
+
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`server running at port ${port} version 7`);
+});
